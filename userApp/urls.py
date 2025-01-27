367e962bfdb34a2c41001ffe11d7965c9ef5abcf
Arise Damilare <arisedamilare5@gmail.com>
2025-01-27 13:22:51 +0100

    Add user profile management with profile model, views, and templates; enhance navigation for authenticated users

--- userApp/urls.py
+++ userApp/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    # path('profile/<int:user_id>/', handleProfile, name='profile'),
 ]
--- userApp/views.py
+++ userApp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.urls import reverse_lazy
 from .forms import SignupForm
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
@@ -9,3 +11,13 @@ class SignUpView(generic.CreateView):
     form_class = SignupForm
     success_url  = reverse_lazy("login")
     template_name = 'registration/signup.html'
+    
+    
+# def handleProfile(request, user_id):
+#     user = get_object_or_404(User, id=user_id)
+    
+#     if request.method == 'POST':
+#         pass
+#     else:
+#         profile_form = ProfileForm(instance=user)
+#         return render(request, template_name='profile.html',  context={'profile_form': profile_form})
